35f110b3de2f4239aaf8fb45ecabc18bd3112c80
luohy15 <luohycs@gmail.com>
2026-02-18 04:31:55 +0000

    Show default shared chat as landing page for unauthenticated users

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -8,8 +8,8 @@ updateFavicon();
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/s/:shareId" element={<ShareView />} />
-      <Route path="/share/:shareId" element={<ShareView />} />
+      <Route path="/s/:shareId" element={<div className="h-dvh flex flex-col overflow-hidden"><ShareView /></div>} />
+      <Route path="/share/:shareId" element={<div className="h-dvh flex flex-col overflow-hidden"><ShareView /></div>} />
       <Route path="/*" element={<App />} />
     </Routes>
   </BrowserRouter>
