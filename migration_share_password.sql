5ef2a854b341a15792e6abdb6a4a953209712cc6
luohy15 <luohycs@gmail.com>
2026-04-23 07:58:51 +0000

    ignore-migration-sql

--- migration_share_password.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add optional password protection to chat and trace shares
--- Run manually with: psql $DATABASE_URL -f migration_share_password.sql
-
-ALTER TABLE chat ADD COLUMN share_password_hash VARCHAR;
-ALTER TABLE trace_share ADD COLUMN password_hash VARCHAR;
