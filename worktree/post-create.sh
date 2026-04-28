41b07887d0a144dde835876967c9cb284f5f3f24
luohy15 <luohycs@gmail.com>
2026-04-28 10:11:56 +0000

    1932-shared-migration

--- worktree/post-create.sh
+++ worktree/post-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for item in web/node_modules web/.env.local .env .venv; do
+for item in web/node_modules web/.env.local .env .venv migration; do
   ln -sfn /Users/roy/luohy15/code/y-agent/$item $item
 done
